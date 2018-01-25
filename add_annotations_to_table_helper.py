374fc2dd82c56fa827c45a00a41146e55935b77e
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-25 16:46:22 -0600

    Fix bug that would fail if the variant does not have a CSQ field

--- add_annotations_to_table_helper.py
+++ add_annotations_to_table_helper.py
@@ -93,7 +93,7 @@ for variant in vcf_file:
     alleles_dict = resolve_alleles(variant, transcripts.keys())
     for alt in alts:
         if alt not in vep[chr][pos][ref]:
-            if transcripts is not None:
+            if transcripts is not None and alleles_dict[alt] in transcripts:
                 vep[chr][pos][ref][alt] = transcript_for_alt(transcripts, alleles_dict[alt])
             else:
                 vep[chr][pos][ref][alt] = None
